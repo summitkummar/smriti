ad3b2a9d940488d4b4d0eb9e9dfa205cd9a59f50
summitkummar <summitenterprises27@gmail.com>
2026-09-19 09:47:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smriti-v7';
+const CACHE = 'smriti-v8';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
