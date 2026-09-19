9a14856aea87885b705b6b0a416471961ea3af04
summitkummar <summitenterprises27@gmail.com>
2026-09-19 09:16:07 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'smriti-v6';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
